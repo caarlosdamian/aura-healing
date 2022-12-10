583027474e64665c59aebb5ac3ed6b9f607342ea
caarlosdamian <dapecarlos@gmail.com>
2022-12-10 08:22:18 -0600

    added header

---
+++ src/sections/header/Header.tsx
@@ -0,0 +1,44 @@
+import "./Header.scss";
+import Hambuerger from "../../images/icon-hamburger.svg";
+import Logo from "../../images/aura.png";
+import { useState } from "react";
+
+export const Header = () => {
+  const [isOpen, setisOpen] = useState(false);
+  return (
+    <header className="header-container">
+      <img src={Logo} alt="logo-img" className="header-logo" />
+      <img
+        src={Hambuerger}
+        alt="hambuerger-menu"
+        className="header-hambuerger-menu"
+        onClick={() => setisOpen(!isOpen)}
+      />
+
+      <nav className="nav-container">
+        <span className="navItem">Productos</span>
+        <span className="navItem">Contacto</span>
+        <button className="menu-button button">PAQUETES</button>
+      </nav>
+      {isOpen && (
+        <div className="menu-container">
+          <a
+            onClick={() => setisOpen(false)}
+            href="#productos"
+            className="menu-option"
+          >
+            PRODUCTOS
+          </a>
+          <a
+            onClick={() => setisOpen(false)}
+            href="#contact"
+            className="menu-option"
+          >
+            CONTACTO
+          </a>
+          <button className="menu-button button">PAQUETES</button>
+        </div>
+      )}
+    </header>
+  );
+};
