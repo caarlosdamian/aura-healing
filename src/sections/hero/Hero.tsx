199ba5ebd9d36b7a8da5d09f38e23d072b4de45d
caarlosdamian <dapecarlos@gmail.com>
2022-12-10 16:21:56 -0600

    refactor added size hook

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import './Hero.scss'
+import React, { useEffect } from "react";
+import { useWindowSize } from "../../hooks/useWindowResize";
+// import HeroImg from '../../images/'
+import "./Hero.scss";
 
 export const Hero = () => {
+  const {width} = useWindowSize()
+  console.log(width)
   return (
-    <section id='hero'>.Hero</section>
-  )
-}
+    <section id="hero">
+      <img src="" alt="" />
+    </section>
+  );
+};
