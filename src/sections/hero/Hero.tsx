c37c29fa00b6b6058d0324154805f22b419b32aa
caarlosdamian <dapecarlos@gmail.com>
2023-01-10 14:23:06 -0600

    version 1.1

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -5,19 +5,22 @@ import "./Hero.scss";
 
 export const Hero = () => {
   const { width } = useWindowSize();
-  console.log(width);
   return (
     <section id="hero" className="hero-container">
       <img
-        src={width > 600 ? HeroImgDestop : HeroImgMobile}
+        src="https://i.ibb.co/wNY629Q/IMG-0123.jpg"
         alt="hero-img"
         className="hero-img"
       />
-      <hr  className="hero-line"/>
+      <hr className="hero-line" />
       <div className="hero-content">
         <h1 className="hero-title">Terapias de sanacion energetica.</h1>
         <h3 className="hero-subtitle">
-        La terapia energética es una técnica que consiste en eliminar la memoria celular a través de la energía humana promoviendo la salud, el balance y la relajación. Esta terapia se basa en la conexión entre el cuerpo y la mente, así como entre nuestras emociones y nuestro espíritu y la influencia que todo ello tiene en nuestras vidas.
+          La terapia energética es una técnica que consiste en eliminar la
+          memoria celular a través de la energía humana promoviendo la salud, el
+          balance y la relajación. Esta terapia se basa en la conexión entre el
+          cuerpo y la mente, así como entre nuestras emociones y nuestro
+          espíritu y la influencia que todo ello tiene en nuestras vidas.
         </h3>
       </div>
     </section>
